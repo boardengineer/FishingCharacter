3589cfa0737298c37ef8624de1c7f47152444c20
boardengineer <77598527+boardengineer@users.noreply.github.com>
2022-09-11 21:29:22 -0700

    Update RNG for StormCompletionAction
    DO NOT MERGE THIS INTO MASTER SINCE IT UPDATES POM.XML for my uses

--- src/main/java/theFishing/actions/StormCompletionAction.java
+++ src/main/java/theFishing/actions/StormCompletionAction.java
@@ -2,7 +2,6 @@ package theFishing.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -15,7 +14,7 @@ public class StormCompletionAction extends AbstractGameAction {
     @Override
     public void update() {
         isDone = true;
-        AbstractMonster target = AbstractDungeon.getMonsters().getRandomMonster(true);
+        AbstractMonster target = AbstractDungeon.getRandomMonster();
         att(new LoseHPAction(target, AbstractDungeon.player, 17));
         att(new VFXAction(new LightningEffect(target.drawX, target.drawY), 0.05F));
         att(new SFXAction("ORB_LIGHTNING_EVOKE"));
